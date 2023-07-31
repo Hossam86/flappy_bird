59f9121c60c553e23383a2007d0d92d1caa8fb6f
hossam samir <hossamsamir@outlook.com>
2023-07-31 13:12:16 +0300

    link box collider with play score

---
+++ Assets/MiddlePipeScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class middlePipeScript : MonoBehaviour
+{
+
+    public Logic_Script logic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<Logic_Script>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.add_score();
+    }
+}

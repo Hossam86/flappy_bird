4f17d119c088c86de3354545c50a43f3f7dead42
hossam samir <hossamsamir@outlook.com>
2023-07-31 13:32:25 +0300

    Tag logic script

--- Assets/MiddleTriggerScript.cs
+++ Assets/MiddleTriggerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class middlePipeScript : MonoBehaviour
+public class MiddleTriggerScript : MonoBehaviour
 {
 
     public Logic_Script logic;
